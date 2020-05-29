35a15670eaa512dd3ea64179d8e89df128d69db4
project-repo <cagebreak_signing_key_2@cagebreak>
2020-05-29 16:13:52 +0200

    Fix permission dropping

--- cagebreak.c
+++ cagebreak.c
@@ -88,13 +88,14 @@ sig_chld_handler(int signal) {
 static bool
 drop_permissions(void) {
 	if(getuid() != geteuid() || getgid() != getegid()) {
-		if(setuid(getuid()) != 0 || setgid(getgid()) != 0) {
+		// Set gid before uid
+		if(setgid(getgid()) != 0 || setuid(getuid()) != 0) {
 			wlr_log(WLR_ERROR, "Unable to drop root, refusing to start");
 			return false;
 		}
 	}
 
-	if(setuid(0) != -1) {
+	if(setgid(0) != -1 || setuid(0) != -1) {
 		wlr_log(WLR_ERROR, "Unable to drop root (we shouldn't be able to "
 		                   "restore it after setuid), refusing to start");
 		return false;
