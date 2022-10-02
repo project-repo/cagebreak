3e45e1f5bb70d3dc5472a4139403a5af71e4a48a
project-repo <cagebreak_signing_key_10@project-repo.co>
2022-10-02 11:41:27 +0200

    Remove unused code

--- input.h
+++ input.h
@@ -14,9 +14,6 @@ void
 cg_input_apply_config(struct cg_input_config *config, struct cg_server *server);
 
 bool
-cg_input_reset_libinput_device(struct cg_input_device *device);
-
-bool
 cg_libinput_device_is_builtin(struct cg_input_device *device);
 
 #endif
