432291f7e2ccf7d90da83935b523b24ab8e64b40
project-repo <cagebreak_signing_key_10@project-repo.co>
2023-01-02 13:34:21 +0100

    Adjust printed names of commands

--- keybinding.h
+++ keybinding.h
@@ -11,59 +11,59 @@
 struct cg_server;
 
 #define FOREACH_KEYBINDING(KEYBINDING) \
-	KEYBINDING(KEYBINDING_RUN_COMMAND) /* data.c is the string to execute */ \
-	KEYBINDING(KEYBINDING_CLOSE_VIEW) \
-	KEYBINDING(KEYBINDING_SPLIT_VERTICAL) \
-	KEYBINDING(KEYBINDING_SPLIT_HORIZONTAL) \
-	KEYBINDING(KEYBINDING_CHANGE_TTY) /*data.u is the desired tty*/\
-	KEYBINDING(KEYBINDING_LAYOUT_FULLSCREEN)\
-	KEYBINDING(KEYBINDING_CYCLE_VIEWS)       /* data.b is 0 if forward, 1 if reverse */\
-	KEYBINDING(KEYBINDING_CYCLE_TILES)       /* data.b is 0 if forward, 1 if reverse */\
-	KEYBINDING(KEYBINDING_CYCLE_OUTPUT)      /* data.b is 0 if forward, 1 if reverse */\
-	KEYBINDING(KEYBINDING_CONFIGURE_OUTPUT)  /* data.o_cfg is the desired output */\
+	KEYBINDING(KEYBINDING_RUN_COMMAND,exec) /* data.c is the string to execute */ \
+	KEYBINDING(KEYBINDING_CLOSE_VIEW,close) \
+	KEYBINDING(KEYBINDING_SPLIT_VERTICAL,vsplit) \
+	KEYBINDING(KEYBINDING_SPLIT_HORIZONTAL,hsplit) \
+	KEYBINDING(KEYBINDING_CHANGE_TTY,switchvt) /*data.u is the desired tty*/\
+	KEYBINDING(KEYBINDING_LAYOUT_FULLSCREEN,only)\
+	KEYBINDING(KEYBINDING_CYCLE_VIEWS,cycle_views)       /* data.b is 0 if forward, 1 if reverse */\
+	KEYBINDING(KEYBINDING_CYCLE_TILES,cycle_tiles)       /* data.b is 0 if forward, 1 if reverse */\
+	KEYBINDING(KEYBINDING_CYCLE_OUTPUT,cycle_outputs)      /* data.b is 0 if forward, 1 if reverse */\
+	KEYBINDING(KEYBINDING_CONFIGURE_OUTPUT,output)  /* data.o_cfg is the desired output */\
 \
-	KEYBINDING(KEYBINDING_CONFIGURE_MESSAGE) /* data.m_cfg is the desired config */\
-	KEYBINDING(KEYBINDING_CONFIGURE_INPUT)   /* data.i_cfg is the desired input configuration */\
+	KEYBINDING(KEYBINDING_CONFIGURE_MESSAGE,configure_message) /* data.m_cfg is the desired config */\
+	KEYBINDING(KEYBINDING_CONFIGURE_INPUT,input)   /* data.i_cfg is the desired input configuration */\
 \
-	KEYBINDING(KEYBINDING_QUIT)\
-	KEYBINDING(KEYBINDING_NOOP)\
-	KEYBINDING(KEYBINDING_SWITCH_OUTPUT)          /* data.u is the desired output */\
-	KEYBINDING(KEYBINDING_SWITCH_WORKSPACE)       /* data.u is the desired workspace */\
-	KEYBINDING(KEYBINDING_SWITCH_MODE)            /* data.u is the desired mode */\
-	KEYBINDING(KEYBINDING_SWITCH_DEFAULT_MODE)    /* data.u is the desired mode */\
-	KEYBINDING(KEYBINDING_RESIZE_TILE_HORIZONTAL) /* data.i is the number of pixels to add */\
+	KEYBINDING(KEYBINDING_QUIT,quit)\
+	KEYBINDING(KEYBINDING_NOOP,abort)\
+	KEYBINDING(KEYBINDING_SWITCH_OUTPUT,screen)          /* data.u is the desired output */\
+	KEYBINDING(KEYBINDING_SWITCH_WORKSPACE,workspace)       /* data.u is the desired workspace */\
+	KEYBINDING(KEYBINDING_SWITCH_MODE,mode)            /* data.u is the desired mode */\
+	KEYBINDING(KEYBINDING_SWITCH_DEFAULT_MODE,setmode)    /* data.u is the desired mode */\
+	KEYBINDING(KEYBINDING_RESIZE_TILE_HORIZONTAL,resize_tile_horizontal) /* data.i is the number of pixels to add */\
 \
-	KEYBINDING(KEYBINDING_RESIZE_TILE_VERTICAL) /* data.i is the number of pixels to add to */\
+	KEYBINDING(KEYBINDING_RESIZE_TILE_VERTICAL,resize_tile_vertical) /* data.i is the number of pixels to add to */\
 \
-	KEYBINDING(KEYBINDING_MOVE_VIEW_TO_WORKSPACE)    /* data.u is the desired workspace */\
-	KEYBINDING(KEYBINDING_MOVE_VIEW_TO_OUTPUT)       /* data.u is the desired output */\
-	KEYBINDING(KEYBINDING_MOVE_VIEW_TO_CYCLE_OUTPUT) /* data.b is 0 if forward, 1 if */\
+	KEYBINDING(KEYBINDING_MOVE_VIEW_TO_WORKSPACE,movetoworkspace)    /* data.u is the desired workspace */\
+	KEYBINDING(KEYBINDING_MOVE_VIEW_TO_OUTPUT,movetoscreen)       /* data.u is the desired output */\
+	KEYBINDING(KEYBINDING_MOVE_VIEW_TO_CYCLE_OUTPUT,move_view_to_cycle_output) /* data.b is 0 if forward, 1 if */\
 \
-	KEYBINDING(KEYBINDING_DUMP)\
-	KEYBINDING(KEYBINDING_SHOW_TIME)\
-	KEYBINDING(KEYBINDING_SHOW_INFO)\
-	KEYBINDING(KEYBINDING_DISPLAY_MESSAGE)\
-	KEYBINDING(KEYBINDING_CURSOR)\
+	KEYBINDING(KEYBINDING_DUMP,dump)\
+	KEYBINDING(KEYBINDING_SHOW_TIME,time)\
+	KEYBINDING(KEYBINDING_SHOW_INFO,show_info)\
+	KEYBINDING(KEYBINDING_DISPLAY_MESSAGE,message)\
+	KEYBINDING(KEYBINDING_CURSOR,cursor)\
 \
-	KEYBINDING(KEYBINDING_SWAP_LEFT)\
-	KEYBINDING(KEYBINDING_SWAP_RIGHT)\
-	KEYBINDING(KEYBINDING_SWAP_TOP)\
-	KEYBINDING(KEYBINDING_SWAP_BOTTOM)\
+	KEYBINDING(KEYBINDING_SWAP_LEFT,exchangeleft)\
+	KEYBINDING(KEYBINDING_SWAP_RIGHT,exchangeright)\
+	KEYBINDING(KEYBINDING_SWAP_TOP,exchangeup)\
+	KEYBINDING(KEYBINDING_SWAP_BOTTOM,exchangedown)\
 \
-	KEYBINDING(KEYBINDING_FOCUS_LEFT)\
-	KEYBINDING(KEYBINDING_FOCUS_RIGHT)\
-	KEYBINDING(KEYBINDING_FOCUS_TOP)\
-	KEYBINDING(KEYBINDING_FOCUS_BOTTOM)\
-	KEYBINDING(KEYBINDING_FOCUS_TILE)\
+	KEYBINDING(KEYBINDING_FOCUS_LEFT,focusleft)\
+	KEYBINDING(KEYBINDING_FOCUS_RIGHT,focusright)\
+	KEYBINDING(KEYBINDING_FOCUS_TOP,focusup)\
+	KEYBINDING(KEYBINDING_FOCUS_BOTTOM,focusdown)\
+	KEYBINDING(KEYBINDING_FOCUS_TILE,focus_tile)\
 \
-	KEYBINDING(KEYBINDING_DEFINEKEY)  /* data.kb is the keybinding definition */\
-	KEYBINDING(KEYBINDING_BACKGROUND) /* data.color is the background color */\
-	KEYBINDING(KEYBINDING_DEFINEMODE) /* data.c is the mode name */\
-	KEYBINDING(KEYBINDING_WORKSPACES) /* data.i is the number of workspaces */\
+	KEYBINDING(KEYBINDING_DEFINEKEY,definekey)  /* data.kb is the keybinding definition */\
+	KEYBINDING(KEYBINDING_BACKGROUND,background) /* data.color is the background color */\
+	KEYBINDING(KEYBINDING_DEFINEMODE,definemode) /* data.c is the mode name */\
+	KEYBINDING(KEYBINDING_WORKSPACES,workspaces) /* data.i is the number of workspaces */\
 
 
-#define GENERATE_ENUM(ENUM) ENUM,
-#define GENERATE_STRING(STRING) #STRING,
+#define GENERATE_ENUM(ENUM,NAME) ENUM,
+#define GENERATE_STRING(STRING,NAME) #NAME,
 
 /* Important: if you add a keybinding which uses data.c or requires "free"
  * to be called, don't forget to add it to the function "keybinding_list_free"
