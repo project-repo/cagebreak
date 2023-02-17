8fbcba1c38fecb9ac75e45ca62f28f8201b4032f
project-repo <cagebreak_signing_key_9@project-repo.co>
2023-02-17 21:31:02 +0100

    Apply shellcheck

--- example_scripts/show_workspace_views.sh
+++ example_scripts/show_workspace_views.sh
@@ -19,6 +19,7 @@ do
 		curr_workspace="$(echo "${event}"|jq -r ".outputs.${curr_output}.curr_workspace")"
 		# Print the process names of the view on the current workspace. jq retrieves
 		# their PID and ps is then used to retrieve the process names.
+		# shellcheck disable=2046
 		(echo -n "message ";ps -o comm=Command -p $(echo "${event}"|jq -r ".outputs.${curr_output}.workspaces[$((curr_workspace-1))].views[].pid")|tail +2|sed ':a; N; $!ba; s/\n/||/g') >&3
 		break
 	fi
